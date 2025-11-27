96ac1bd71337f20678ecc993199919330ee272ac
leonardo <gabrielleonardo2512@gmail.com>
2025-11-27 18:15:47 -0300

    commit 9

--- entrega/Movie.java
+++ entrega/Movie.java
@@ -1,8 +1,7 @@
 public class Movie {
-
-    public static final int  CHILDRENS = 2;
-    public static final int  REGULAR = 0;
-    public static final int  NEW_RELEASE = 1;
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
 
     private String _title;
     private int _priceCode;
@@ -20,7 +19,32 @@ public class Movie {
         _priceCode = arg;
     }
 
-    public String getTitle (){
+    public String getTitle() {
         return _title;
+    }
+
+    // 🔥 NOVO MÉTODO (trecho extraído do Rental)
+    public double getCharge(int daysRented) {
+        double result = 0;
+
+        switch (_priceCode) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+
+            case CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return result;
     }
 }
